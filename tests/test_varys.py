6beec385322a13e6059167bcb84611bbf8066953
Warrick Ball <warrickball@gmail.com>
2023-08-24 16:00:58 +0100

    refactor and separate tests, as suggested by Adrian (some tests don't work as standalone functions)

--- tests/test_varys.py
+++ tests/test_varys.py
@@ -7,10 +7,11 @@ from varys import varys
 DIR = os.path.dirname(__file__)
 LOG_FILENAME = os.path.join(DIR, 'test.log')
 TMP_HANDLE, TMP_FILENAME = tempfile.mkstemp()
+TEXT = "Hello, world!"
 
 class TestVarys(unittest.TestCase):
 
-    def test_varys(self):
+    def setUp(self):
         config = {
             "version": "0.1",
             "profiles": {
@@ -26,32 +27,49 @@ class TestVarys(unittest.TestCase):
         with open(TMP_FILENAME, 'w') as f:
             json.dump(config, f, ensure_ascii=False)
 
-        text = "Hello, world!"
+        self.v = varys('test', LOG_FILENAME, config_path=TMP_FILENAME)
 
-        v = varys('test', LOG_FILENAME, config_path=TMP_FILENAME)
-
-        self.assertRaises(Exception, v.send, text, 'basic')
-        self.assertRaises(Exception, v.receive, 'basic', block=False)
-
-        v.send(text, 'basic', queue_suffix='q')
-        message = v.receive('basic', queue_suffix='q')
-        self.assertEqual(text, json.loads(message.body))
-
-        message = v.receive('basic', queue_suffix='q', block=False)
-        v.send(text, 'basic', queue_suffix='q')
-        messages = v.receive_batch('basic', queue_suffix='q')
-
-        self.assertRaises(Exception, v.receive_batch, 'batch', block=False)
-
-        channels = v.get_channels()
+    def tearDown(self):
+        channels = self.v.get_channels()
         for key in channels['consumer_channels']:
             print(f"tearing down consumer {key}")
-            v._varys__in_channels[key]["varys_obj"].close_connection()
-            v._varys__in_channels[key]["varys_obj"].stop()
+            self.v._varys__in_channels[key]["varys_obj"].close_connection()
+            self.v._varys__in_channels[key]["varys_obj"].stop()
 
         for key in channels['producer_channels']:
             print(f"tearing down producer {key}")
-            v._varys__out_channels[key]["varys_obj"].stop()
+            self.v._varys__out_channels[key]["varys_obj"].stop()
+
+        os.remove(TMP_FILENAME)
+
+    def test_send_and_receive(self):
+        self.v.send(TEXT, 'basic', queue_suffix='q')
+        message = self.v.receive('basic', queue_suffix='q')
+        self.assertEqual(TEXT, json.loads(message.body))
+
+    # def test_send_and_receive_batch(self):
+    # hangs if in separate test
+        self.v.send(TEXT, 'basic', queue_suffix='q')
+        messages = self.v.receive_batch('basic', queue_suffix='q')
+
+    # def test_receive_no_message(self):
+    # hangs if in separate test
+        self.assertIsNone(self.v.receive('basic', queue_suffix='q', block=False))
+
+    def test_send_no_suffix(self):
+        self.assertRaises(Exception, self.v.send, TEXT, 'basic')
+
+    def test_receive_no_suffix(self):
+        self.assertRaises(Exception, self.v.receive, 'basic', block=False)
+
+    def test_receive_batch_no_suffix(self):
+        self.assertRaises(Exception, self.v.receive_batch, 'basic')
+
+
+class TestVarysConfig(unittest.TestCase):
+
+    def tearDown(self):
+        os.remove(TMP_FILENAME)
 
     def test_config_not_json(self):
         with open(TMP_FILENAME, 'w') as f:
@@ -62,8 +80,6 @@ class TestVarys(unittest.TestCase):
             v = varys('test', LOG_FILENAME, config_path=TMP_FILENAME)
 
         self.assertEqual(cm.exception.code, 11)
-
-        os.remove(TMP_FILENAME)
 
     def test_config_profile_missing(self):
         config = {
@@ -80,8 +96,6 @@ class TestVarys(unittest.TestCase):
             v = varys('test', LOG_FILENAME, config_path=TMP_FILENAME)
             
         self.assertEqual(cm.exception.code, 2)
-
-        os.remove(TMP_FILENAME)
 
     def test_config_profile_incomplete(self):
         config = {
@@ -101,5 +115,3 @@ class TestVarys(unittest.TestCase):
             v = varys('test', LOG_FILENAME, config_path=TMP_FILENAME)
 
         self.assertEqual(cm.exception.code, 11)
-
-        os.remove(TMP_FILENAME)
